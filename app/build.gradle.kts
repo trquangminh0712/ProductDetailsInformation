5a37faf96dd5497c6e9b533d73a34e84ee3b2b1d
Tran Quang Minh <trquangminh0712@gmail.com>
2026-08-09 17:02:59 +0700

    Edittext and button added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.quangminh.productdetailsrinformation"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
