add0c3ddd7f20cb21e7719626c8e15f616cab638
Tran Quang Minh <trquangminh0712@gmail.com>
2026-08-10 22:07:49 +0700

    1st part of UI

--- app/src/main/java/com/quangminh/productdetailsrinformation/MainActivity.kt
+++ app/src/main/java/com/quangminh/productdetailsrinformation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.quangminh.productdetailsrinformation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Paint
+import android.widget.TextView
+import android.text.SpannableString
+import android.text.style.StrikethroughSpan
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.before_discount_price)
+        val textContent = getString(R.string._10_76)
+        val spannable = android.text.SpannableString(textContent)
+        spannable.setSpan(android.text.style.StrikethroughSpan(), 0, textContent.length, 0)
+        textView.text = spannable
+
+
     }
+
+
 
 }
