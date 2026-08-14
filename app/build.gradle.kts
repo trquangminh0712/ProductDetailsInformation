03ac74aac4392968f848a9eda6c7c3e43a85b93a
Tran Quang Minh <trquangminh0712@gmail.com>
2026-08-14 20:20:59 +0700

    2nd part of Api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
---
+++ app/src/main/java/com/quangminh/productdetailsrinformation/CurrentProductDetails.kt
@@ -0,0 +1,21 @@
+package com.quangminh.productdetailsrinformation
+
+import com.google.android.libraries.places.api.model.Review
+
+data class CurrentProductDetails(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Double,
+    val discountPercentage: Double,
+    val rating: Double,
+    val stock: Int,
+    val brand: String,
+    val reviews: List<Review> = emptyList()
+)
+data class Reviews(
+    val rating: Double,
+    val comment: String,
+    val reviewerName: String
+
+)
